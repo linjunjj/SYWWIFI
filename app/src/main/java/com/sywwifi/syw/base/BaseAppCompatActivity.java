dffc556c01fc1ce725aaa8ad5162dbb69c392e9d
linjun <10454735402@qq.com>
2017-07-01 18:40:54 +0800

    add BaseAppCompatActivity

--- app/src/main/java/com/sywwifi/syw/base/BaseAppCompatActivity.java
+++ app/src/main/java/com/sywwifi/syw/base/BaseAppCompatActivity.java
@@ -1,9 +1,12 @@
 package com.sywwifi.syw.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by linjun on 2017/7/1.
@@ -24,5 +27,16 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
         setContentView(getLayoutId());
         getLayoutId();
     }
+private void FullScreen(){
+    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+        Window window=getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        );
 
+    }
+
+
+}
 }
