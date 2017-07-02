1deacfeb280fc62647582bd241df78aa1e38aa49
linjunjj <1045735402@qq.com>
2017-07-02 09:49:42 +0800

    Add Tinker

--- app/src/main/java/com/sywwifi/syw/base/App.java
+++ app/src/main/java/com/sywwifi/syw/base/App.java
@@ -2,7 +2,12 @@ package com.sywwifi.syw.base;
 
 import android.app.Application;
 
+import com.sywwifi.syw.Handler.FetchPatchHandler;
+import com.sywwifi.syw.constant.BuildConfig;
 import com.sywwifi.syw.utils.OkHttpUtil;
+import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tinkerpatch.sdk.TinkerPatch;
+import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
 
 /**
  * Created by shaowen on 2017/6/26.
@@ -10,19 +15,36 @@ import com.sywwifi.syw.utils.OkHttpUtil;
 
 public  class App extends Application {
     private static App app;
-
+    private ApplicationLike tinkerApplicationLike;
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
         //初始化
         init();
+
     }
 
     private void init(){
         OkHttpUtil.initOkHttp(this);
     }
+   private  void initTink(){
 
+       if (BuildConfig.TINKER_ENABLE) {
+
+           // 我们可以从这里获得Tinker加载过程的信息
+           tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
+
+           // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
+           TinkerPatch.init(tinkerApplicationLike)
+                   .reflectPatchLibrary()
+                   .setPatchRollbackOnScreenOff(true)
+                   .setPatchRestartOnSrceenOff(true);
+
+           // 每隔3个小时去访问后台时候有更新,通过handler实现轮训的效果
+           new FetchPatchHandler().fetchPatchWithInterval(3);
+       }
+   }
 
 
     public static App getApp()
---
+++ app/src/main/java/com/sywwifi/syw/constant/BuildConfig.java
@@ -0,0 +1,16 @@
+package com.sywwifi.syw.constant;
+
+/**
+ * Created by linjun on 2017/7/2.
+ */
+
+public class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.example.gavin.tinkertest";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "4.0.0";
+    // Fields from default config.
+    public static final boolean TINKER_ENABLE = true;
+}
